b38bafd69d1853da4ce487d0c47993206494043f
jwiggins <jwiggins@enthought.com>
2012-01-30 17:44:36 -0600

    Add empty implementations for WPFWindow and WPFMainWindow.

---
+++ enaml/widgets/wpf/wpf_main_window.py
@@ -0,0 +1,81 @@
+#------------------------------------------------------------------------------
+#  Copyright (c) 2012, Enthought, Inc.
+#  All rights reserved.
+#------------------------------------------------------------------------------
+import wpyf
+from .wpf_window import WPFWIndow
+
+from ..main_window import AbstractTkMainWindow
+
+
+class WPFMainWindow(WPFWindow, AbstractTkMainWindow):
+    """ A WPF implementation of a MainWindow.
+
+    """
+    #--------------------------------------------------------------------------
+    # Setup methods
+    #--------------------------------------------------------------------------
+    def create(self, parent):
+        """ Creates the underlying WPFMainWindow object.
+
+        """
+        self.widget = None
+
+    def initialize(self):
+        """ Initialize the WPFMainWindow object.
+
+        """
+        super(WPFMainWindow, self).initialize()
+        self.update_menu_bar()
+
+    #--------------------------------------------------------------------------
+    # Change Handlers
+    #--------------------------------------------------------------------------
+    def shell_menu_bar_changed(self, menu_bar):
+        """ Update the menu bar of the window with the new value from
+        the shell object.
+
+        """
+        self.update_menu_bar()
+
+    #--------------------------------------------------------------------------
+    # Abstract Implementation
+    #--------------------------------------------------------------------------
+    def menu_bar_height(self):
+        """ Returns the height of the menu bar in pixels. If the menu
+        bar does not have an effect on the height of the main window,
+        this method returns Zero.
+
+        """
+        return 0
+
+    #--------------------------------------------------------------------------
+    # Widget Update Methods
+    #--------------------------------------------------------------------------
+    def update_menu_bar(self):
+        """ Updates the menu bar in the main window with the value
+        from the shell object.
+
+        """
+        menu_bar = self.shell_obj.menu_bar
+
+    def update_central_widget(self):
+        """ Updates the central widget in the main window with the 
+        value from the shell object.
+
+        """
+        # It's possible for the central widget component to be None.
+        # This must be allowed since the central widget may be generated
+        # by an Include component, in which case it will not exist 
+        # during initialization. However, we must have a central widget
+        # for the MainWindow, and in that case we just fill it with a
+        # dummy widget.
+        central_widget = self.shell_obj.central_widget
+
+    def set_visible(self, visible):
+        """ Overridden from the parent class to raise the window to
+        the front if it should be shown.
+
+        """
+        pass
+
---
+++ enaml/widgets/wpf/wpf_window.py
@@ -0,0 +1,84 @@
+#------------------------------------------------------------------------------
+#  Copyright (c) 2012, Enthought, Inc.
+#  All rights reserved.
+#------------------------------------------------------------------------------
+
+import wpyf
+
+from ..window import AbstractTkWindow
+
+
+class WPFWindow(AbstractTkWindow):
+    """ A WPF implementation of a Window. It serves as a base class for 
+    WPFMainWindow and WPFDialog. It is not meant to be used directly.
+
+    """
+    #--------------------------------------------------------------------------
+    # Setup methods
+    #--------------------------------------------------------------------------
+    def create(self, parent):
+        """ Create the underlying WPF widget.
+
+        """
+        msg = 'A WPFWindow is a base class and cannot be used directly'
+        raise NotImplementedError(msg)
+
+    def initialize(self):
+        """ Intializes the attributes on the underlying WPF widget.
+
+        """
+        super(WPFWindow, self).initialize()
+        self.set_title(self.shell_obj.title)
+        self.update_central_widget()
+
+    #--------------------------------------------------------------------------
+    # Implementation
+    #--------------------------------------------------------------------------
+    def maximize(self):
+        """ Maximizes the window to fill the screen.
+
+        """
+        pass
+            
+    def minimize(self):
+        """ Minimizes the window to the task bar.
+
+        """
+        pass
+            
+    def normalize(self):
+        """ Restores the window after it has been minimized or maximized.
+
+        """
+        pass
+
+    def shell_title_changed(self, title):
+        """ The change handler for the 'title' attribute on the shell
+        object.
+
+        """
+        self.set_title(title)
+    
+    def shell_central_widget_changed(self, central_widget):
+        """ The change handler for the 'central_widget' attribute on 
+        the shell object.
+
+        """
+        self.update_central_widget()
+    
+    #--------------------------------------------------------------------------
+    # Widget Update Methods 
+    #--------------------------------------------------------------------------
+    def update_central_widget(self):
+        """ Updates the central widget from the value on the shell 
+        object. This method must be implemented by subclasses.
+
+        """
+        raise NotImplementedError
+
+    def set_title(self, title):
+        """ Sets the title of the underlying widget.
+
+        """
+        pass
+
